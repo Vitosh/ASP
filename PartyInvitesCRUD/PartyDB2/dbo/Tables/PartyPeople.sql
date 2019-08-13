86f8fa0375058a0c8a45aae8ef66cf84dca8f2c3
unknown <vitosh.doynov@gmail.com>
2019-08-13 22:48:59 +0200

    removing CRUD

--- PartyInvitesCRUD/PartyDB2/dbo/Tables/PartyPeople.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PartyPeople]
-(
-	[Id] INT NOT NULL  IDENTITY(1, 1), 
-    [NickName] NVARCHAR(50) NULL, 
-    [FancyMail] NVARCHAR(50) NULL, 
-    [FavouriteAnimal] NVARCHAR(50) NULL, 
-    [AnimalName] NVARCHAR(50) NULL, 
-    CONSTRAINT [PK_PartyPeople] PRIMARY KEY ([Id]) 
-)
--- PartyInvitesCRUD/ReadMe.md
+++
@@ -1,4 +0,0 @@
-## Article:
-[VitoshAcademy.com](https://www.vitoshacademy.com/asp-data-entry-mvc-application-with-database-video)
-## Video:
-[YouTube](https://www.youtube.com/watch?v=NbKeazpW1Ik)
