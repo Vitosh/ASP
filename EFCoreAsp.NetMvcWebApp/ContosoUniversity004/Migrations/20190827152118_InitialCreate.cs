88cf59e54abf1825bea20fa9bf7e6ba1b691f54c
unknown <vitosh.doynov@gmail.com>
2019-08-27 18:24:31 +0200

    004

---
+++ EFCoreAsp.NetMvcWebApp/ContosoUniversity004/Migrations/20190827152118_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Credits = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CourseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LastName = table.Column<string>(nullable: true),
+                    FirstMidName = table.Column<string>(nullable: true),
+                    EnrollmentDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiantes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inscriptiones",
+                columns: table => new
+                {
+                    EnrollmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CourseId = table.Column<int>(nullable: false),
+                    StudentId = table.Column<int>(nullable: false),
+                    Grade = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscriptiones", x => x.EnrollmentId);
+                    table.ForeignKey(
+                        name: "FK_Inscriptiones_Cursos_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Cursos",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Inscriptiones_Estudiantes_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Estudiantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscriptiones_CourseId",
+                table: "Inscriptiones",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscriptiones_StudentId",
+                table: "Inscriptiones",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscriptiones");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "Estudiantes");
+        }
+    }
+}
---
+++ EFCoreAsp.NetMvcWebApp/ContosoUniversity004/Migrations/20190827152603_Kursove.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class Kursove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inscriptiones_Cursos_CourseId",
+                table: "Inscriptiones");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cursos",
+                table: "Cursos");
+
+            migrationBuilder.RenameTable(
+                name: "Cursos",
+                newName: "Kursove");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Kursove",
+                table: "Kursove",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inscriptiones_Kursove_CourseId",
+                table: "Inscriptiones",
+                column: "CourseId",
+                principalTable: "Kursove",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Inscriptiones_Kursove_CourseId",
+                table: "Inscriptiones");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Kursove",
+                table: "Kursove");
+
+            migrationBuilder.RenameTable(
+                name: "Kursove",
+                newName: "Cursos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cursos",
+                table: "Cursos",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Inscriptiones_Cursos_CourseId",
+                table: "Inscriptiones",
+                column: "CourseId",
+                principalTable: "Cursos",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
